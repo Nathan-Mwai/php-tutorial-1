101c0dfca32e9e5c3217980b40fc3e9852d18490
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-04-28 11:57:29 +0300

    implement get global variable. Implement  request global variable

--- index.php
+++ index.php
@@ -7,13 +7,8 @@
 </head>
 <body>
     <?php
-    echo $_SERVER["DOCUMENT_ROOT"];
-    echo "<br>";
-    echo $_SERVER["PHP_SELF"];
-    echo "<br>";
-    echo $_SERVER["SERVER_NAME"];
-    echo "<br>";
-    echo $_SERVER["REQUEST_METHOD"];
+    // echo $_GET["name"];
+    echo $_REQUEST["name"];
     ?>
 </body>
 </html>
