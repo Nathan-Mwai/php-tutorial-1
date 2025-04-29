4f45dfdce31aafda7c31b7b282f70ba30b6564cd
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-04-29 23:01:46 +0300

    Made a simple form

--- index.php
+++ index.php
@@ -6,15 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    $_SERVER[""];
-    $_GET[""];
-    $_POST[""];
-    $_REQUEST[""];
-    $_FILES[""];
-    $_COOKIE[""];
-    $_SESSION[""];
-    $_ENV[""]
-    ?>
+    <main>
+        <form action="formhandler.php">
+            <label for="firstname">Firstname?</label>
+            <input type="text" name="firstname" id="firstname" placeholder="Enter Your First Name"/>
+
+            <label for="lastname">Lastname?</label>
+            <input type="text" name="lastname" id="lastname" placeholder="Enter your last name"/>
+
+            <label for="favouritepet"> Favourite pet?</label>
+            <select name="favouritepet" id="favouritepet">
+                <option value="none">None</option>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="bird">Bird</option>
+            </select>
+            <button type="submit">Submit</button>
+        </form>
+    </main>
 </body>
 </html>
