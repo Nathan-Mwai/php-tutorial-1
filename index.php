6b9a81be53c42fe1032a9c63c48df950d7dbd1de
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-04-29 23:32:24 +0300

    Implement string operation using concatenation

--- index.php
+++ index.php
@@ -6,26 +6,13 @@
     <title>Document</title>
 </head>
 <body>
-    <main>
-        <form action="form/formhandler.php" method="post">
-            <label for="firstname">Firstname?</label>
-            <!-- The required is ot a guaranteeon security against submitting empty inputs. To prevent this I use server side security -->
-            <input required type="text" name="firstname" id="firstname" placeholder="Enter Your First Name"/>
+    <?php
+    // String operator
 
-            <label for="lastname">Lastname?</label>
-            <input type="text" name="lastname" id="lastname" placeholder="Enter your last name"/>
-
-            <label for="favouritepet"> Favourite pet?</label>
-            <select name="favouritepet" id="favouritepet">
-                <option value="none">None</option>
-                <option value="dog">Dog</option>
-                <option value="cat">Cat</option>
-                <option value="bird">Bird</option>
-            </select>
-            <!-- name="submit" was used with isset -->
-             <button type="submit">Submit</button>
-
-        </form>
-    </main>
+    $a = "Hello";
+    $b = "World!";
+    $c = $a . " " . $b;
+    echo $c;
+    ?>
 </body>
 </html>
