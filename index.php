8cb5f2f39cbdbed4f9a4005d26b245ee18e64dc9
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-04-29 23:18:15 +0300

    Implement incrementing and decrementing operators

--- index.php
+++ index.php
@@ -7,19 +7,15 @@
 </head>
 <body>
     <?php
-    // logical operators
-
-    //we use "and" === &&
-    // we use "or" === ||
-    $b = 4;
-    $a = 4;
-
-    $c = 5;
-    $d = 4;
-    if($a == $b || $c == $d) {
-        echo "True statement";
-        
-    }
+    // incrementing/decrementing operators
+    // ++a increases by 1
+    // --a decreases by 1
+    // a++ We add one to the variable but it won't be seen on the view. The original will be seen i.e if a = 1 then a++ = 1. However when we call tha variable below the v++ the "v" will return the v + v making it 2v 
+    $a = 2;
+    // a-- We subtract one to the variable but it won't be seen on the view. The original will be seen i.e if a = 1 then a-- = 1. However when we call tha variable below the v-- the "v" will return the v - v making it 0v 
+    $a = 2;
+    echo $a--;
+    echo $a;
     ?>
 </body>
 </html>
