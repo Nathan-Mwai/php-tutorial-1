3fe144f26284491ee81ded9f63835704cb53c63e
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-05-01 12:31:21 +0300

    Implement initial array understanding

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-    // switch and if statements are similar to js
-    $a= "1";
-    $b=4;
+    $fruits = ["Apple", "Banana", "Pineapple"];
 
-    $result = match ($a) {
-        // 1,2,3,4,5,6,7, this is how we use "or" using a comma
-        // it is strict on data types comparison
-        1 => "Variable a is equal to 1",
-        2 => "Variable a is equal to 2",
-        //default / else
-        default => "None were a match",
-    };
+    $fruits[] = "Orange";
 
-    echo $result
+    echo $fruits[3];
+    // This one replaces the banana to avocado
+    $fruits[1] = "Avocado";
+    echo $fruits[1];
+
+    //Delete a fruit without reseting the count i.e 1 remains empty and throws an error
+    // unset($fruits[1]);
+    // echo $fruits[1];
+
+    // Delete a fruit and reconfigure the count
+    array_splice($fruits,0,1); // Takes out banana
+    echo $fruits[1];
     ?>
 </body>
+
 </html>
